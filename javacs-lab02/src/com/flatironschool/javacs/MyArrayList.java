b6f3b2640609e3316422623c3fc0f83efe18b6d6
emily-nguyen <emily-nguyen@users.noreply.github.com>
2016-04-10 06:07:25 -0700

    Implemented ArrayList functions: get, indexOf, add, and remove

--- javacs-lab02/src/com/flatironschool/javacs/MyArrayList.java
+++ javacs-lab02/src/com/flatironschool/javacs/MyArrayList.java
@@ -62,7 +62,14 @@ public class MyArrayList<E> implements List<E> {
 		if (index < 0 || index > size) {
 			throw new IndexOutOfBoundsException();
 		}
-		// TODO: fill in the rest of this method
+
+        add(element);
+
+        for (int i=size-1; i > index; i--) {
+            array[i] = array[i-1];
+        }
+
+        array[index] = element;
 	}
 
 	@Override
@@ -111,8 +118,13 @@ public class MyArrayList<E> implements List<E> {
 
 	@Override
 	public int indexOf(Object target) {
-		// TODO: fill in this method
-		return 0;
+		for (int i=0; i < size; i++) {
+            if (equals(target, array[i])) {
+                return i;
+            }
+        }
+
+        return -1; 
 	}
 
 	/** Checks whether an element of the array is the target.
@@ -182,8 +194,15 @@ public class MyArrayList<E> implements List<E> {
 
 	@Override
 	public E remove(int index) {
-		// TODO: fill in this method.
-		return null;
+        E element = get(index);
+
+        for (int i=index; i < size; i++) {
+            array[i] = array[i+1];
+        }
+
+        size--; 
+
+        return element; 
 	}
 
 	@Override
@@ -202,8 +221,9 @@ public class MyArrayList<E> implements List<E> {
 
 	@Override
 	public E set(int index, E element) {
-		// TODO: fill in this method.
-		return null;
+	    E oldElement = get(index);
+        array[index] = element; 
+        return oldElement;
 	}
 
 	@Override
